55e1d1bb73376aea477e0fb385ac934d05cff7c0
linwenjun <linwenjun@163.com>
2020-07-14 22:12:02 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const faker = require('faker');
+const YAML = require('yaml');
+const fs = require('fs');
+
+const file = fs.readFileSync('./config.yaml', 'utf8');
+const config = YAML.parse(file);
+const count = parseInt(config.count) || 1;
+faker.locale = "zh_CN";
+
+let fields = (Object.keys(config.entries));
+
+
+
+let result = {data: []}
+for(let i=0; i< count; i++) {
+  result.data.push(genEntry());
+}
+
+let jsonStr = JSON.stringify(result, true, 2);
+
+console.log(jsonStr);
+
+function genEntry() {
+  let result = {};
+  for(let i=0; i<fields.length; i++) {
+    let key = fields[i];
+    let value = faker.fake(config.entries[fields[i]]);
+    result[key] = value;  
+  }
+  return result;
+}
